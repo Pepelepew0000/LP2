a76a8ca27217e3e54562bd3da0b593407c981d3f
Pepelepew0000 <69858101+Pepelepew0000@users.noreply.github.com>
2021-06-22 12:48:25 -0300

    Add files via upload

---
+++ PMat+0030482011049/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PMat_0030482011049
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EXE = new System.Windows.Forms.Button();
+            this.LBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // EXE
+            // 
+            this.EXE.Location = new System.Drawing.Point(126, 124);
+            this.EXE.Name = "EXE";
+            this.EXE.Size = new System.Drawing.Size(75, 23);
+            this.EXE.TabIndex = 0;
+            this.EXE.Text = "Ok";
+            this.EXE.UseVisualStyleBackColor = true;
+            this.EXE.Click += new System.EventHandler(this.EXE_Click);
+            // 
+            // LBox
+            // 
+            this.LBox.FormattingEnabled = true;
+            this.LBox.Location = new System.Drawing.Point(385, 20);
+            this.LBox.Name = "LBox";
+            this.LBox.Size = new System.Drawing.Size(376, 537);
+            this.LBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 593);
+            this.Controls.Add(this.LBox);
+            this.Controls.Add(this.EXE);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EXE;
+        private System.Windows.Forms.ListBox LBox;
+    }
+}
+
---
+++ PMat+0030482011049/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PMat_0030482011049
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EXE_Click(object sender, EventArgs e)
+        {
+            float[] A = new float[10];
+            float[] B = new float[10];
+            int I=0;
+            string message= "INSIRA UM VALOR NUMERICO ATÉ O FIM DAS MSGS";
+            string title = "Entrada de valores";
+            string D;
+            object myvalue;
+
+            
+
+
+
+            for (I = 0; I < 10; I++)
+            {
+                myvalue = Interaction.InputBox(title, message);
+                D = myvalue.ToString();
+                MessageBox.Show(D);
+                A[I] = float.Parse(D);
+            }
+
+            I = 0;
+            
+
+            while( I<10 )
+            {
+                D =("Vetor A " + A[I].ToString("0.##"));
+                LBox.Items.Add(D);
+                if (I % 2 == 0)
+                {
+                    B[I] = A[I] * 5;
+                }
+                else
+                {
+                    B[I] = A[I] + 5;
+                }
+                D = ("Vetor B "+ B[I].ToString("0.##"));
+                LBox.Items.Add(D);
+                I++;
+            }
+
+
+        }
+    }
+}
